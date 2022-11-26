f6e4168db23c3903f681e7fc6dd9dfb8f33ff2ef
TimVladislav13010 <vladislav130ua1@gmail.com>
2022-11-26 09:36:33 +0200

    save

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-#
+from collections import UserDict
+
+
+"""
+Бот помічник.
+Працює з командами (hello, add, change, phone, show_all, good_bye, close, exit, .)
+"""
+
+ADDRESSBOOK = {"Vlad": ["0687417002"]}
+
+
+class AddressBook(UserDict):
+    """
+    Книга контактів.
+    """
+
+
+class Record:
+    pass
+
+
+class Field:
+    pass
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    pass
+
+
+if __name__ == "__main__":
+    user_1 = AddressBook(AddressBook)
+    print(user_1)
